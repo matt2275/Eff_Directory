7d3e0d363372ca199786b66414b977959ee38fa0
matt2275 <54369650+matt2275@users.noreply.github.com>
2021-03-23 11:33:41 -0500

    Update continuum_crab.py

--- effAnalyzer/python/continuum_crab.py
+++ effAnalyzer/python/continuum_crab.py
@@ -3,10 +3,10 @@ config = Configuration()
 config.section_('General')
 config.General.transferLogs = False
 config.General.transferOutputs = True
-config.General.workArea = 'HI_UPC_continuum'
+config.General.workArea = 'HI_Forward_Continuum'
 config.General.requestName = 'HLT_Efficiency'
 config.section_('JobType')
-config.JobType.psetName = '/eos/user/m/mumuhamm/CMSSW_10_3_3_patch1/src/Eff_Directory/effAnalyzer/python/eff_muon_data.py'
+config.JobType.psetName = '/afs/cern.ch/user/m/mnickel/private/EFF_ANALYZER/CMSSW_10_3_3_patch1/src/Eff_Directory/effAnalyzer/python/eff_muon_data.py'
 config.JobType.pluginName = 'Analysis'
 config.JobType.allowUndistributedCMSSW = True
 config.section_('Data')
@@ -15,10 +15,10 @@ config.Data.inputDataset = '/HIForward/HIRun2018A-04Apr2019-v1/AOD'
 config.Data.ignoreLocality = False
 config.Data.unitsPerJob     = 200#5000
 config.Data.totalUnits      = 700000
-config.Data.splitting       ='EventAwareLumiBased'#'LumiBased'#'FileBased'#EventAwareLumiBased'
-config.JobType.maxMemoryMB = 2500
+config.Data.splitting       ='EventAwareLumiBased'#'LumiBased'#'FileBased'#EventAwareLumiBased'Automatic
+#config.JobType.maxMemoryMB = 2500
 config.Data.lumiMask = 'Cert_326381-327564_HI_PromptReco_Collisions18_JSON_HF_and_MuonPhys.txt'
-config.Data.outLFNDirBase = '/store/user/mumuhamm/ForwardData'
+config.Data.outLFNDirBase = '/store/user/mnickel/TauTau/Crab'  #outfile Location
 config.Data.publication = False
 config.section_('Site')
-config.Site.storageSite = 'T3_CH_CERNBOX'
+config.Site.storageSite = 'T3_CH_CERNBOX'  #needed to save to cernbox 
